54ec6900f5f89c84e5e9f050ea133e76e178e4e2
Spencer <wadsworth.spencer.g@gmail.com>
2025-08-04 09:17:05 -0400

    add some score plots

--- make_images/make_final_analysis_images.R
+++ make_images/make_final_analysis_images.R
@@ -1,10 +1,24 @@
-source("./flu_forecast_23/get_data_functions_new.R")
-team_scores <- readRDS("./flu_forecast_23/hub_scores_no_ens.rds")
-ili_scores <- readRDS("./flu_forecast_23/nl_scores.rds")
+source("../flu_forecast_23/get_data_functions_new.R")
+library(datasets)
+state.name <- c(state.name, "Puerto Rico", "District of Columbia", "US")
+state.abb <- c(state.abb, "PR", "DC", "US")
+states <- data.frame(loc_abb = state.abb, location_name = state.name)
+
+
+team_scores <- readRDS("../flu_forecast_23/hub_scores_no_ens.rds") %>% 
+  left_join(states, by = "location_name") %>% 
+  mutate(model = ifelse(model == "FluSight-baseline", 
+                        # "<b><i>FluSight-baseline</i></b>", 
+                        "<span style='font-size:12pt'><b><i>FluSight-baseline</i></b></span>",
+                        model))
+ili_scores <- readRDS("../flu_forecast_23/nl_scores.rds") %>% 
+  left_join(states, by = "location_name")
+  
+
 
 rbind(team_scores, ili_scores) %>% 
-  filter(str_detect(model, "asg")) %>%
-  filter(location_name != "Florida") %>% 
+  # filter(str_detect(model, "asg")) %>%
+  # filter(location_name != "Florida") %>% 
   group_by(model) %>% 
   summarise(m = mean(lwis, na.rm = TRUE),
             n = n()) %>% 
@@ -14,71 +28,429 @@ rbind(team_scores, ili_scores) %>%
 rbind(team_scores, ili_scores) %>% 
   filter(model %in% c("asg_disc2_nm_hosp_sq_ar1", "FluSight-baseline")) %>% 
   ggplot() +
-  geom_boxplot(aes(x = factor(horizon), y = wis, colour = model)) +
+  geom_boxplot(aes(x = factor(horizon), y = lwis, colour = model)) #+
   scale_y_log10() 
 
-team_scores <- rbind(team_scores, ili_scores %>% filter(model == "asg_disc2_nm_hosp_sq_ar1"))
+team_scores <- rbind(team_scores, 
+                     ili_scores %>% 
+                       filter(model == "asg_disc2_nm_hosp_sq_ar1")) 
 
 best_nl <- ili_scores %>% 
   filter(model == "asg_disc2_nm_hosp_sq_ar1") %>% 
   mutate(nl_wis = wis, nl_lwis = lwis, nl_mae = mae) %>% 
-  dplyr::select(reference_date, location_name, horizon, nl_wis, nl_lwis, nl_mae)
+  dplyr::select(reference_date, loc_abb, horizon, nl_wis, nl_lwis, nl_mae)
 
 team_scores %>% 
-  left_join(best_nl, by = c("reference_date", "location_name", "horizon")) %>% 
-  group_by(model, reference_date) %>% 
+  left_join(best_nl, by = c("reference_date", "loc_abb", "horizon")) %>% 
+  filter(model %in% c( "FluSight-baseline")) %>% 
+  group_by(model, reference_date, horizon) %>% 
   summarise(mwis = mean(wis, na.rm = TRUE),
             nl_mwis = mean(nl_wis, na.rm = TRUE)) %>% 
   # filter(model == "FluSight-baseline") %>%
   ggplot() +
-  geom_line(aes(x = nl_mwis, y = mwis, colour = model)) +
+  geom_point(aes(x = nl_mwis, y = mwis, colour = model)) +
   stat_function(fun = function(x) x + 0, color = "blue") +
   scale_y_log10() +
-  scale_x_log10()
+  scale_x_log10() +
+  theme(legend.position = "none")
 
 
 team_scores %>% 
-  left_join(best_nl, by = c("reference_date", "location_name", "horizon")) %>% 
-  group_by(model, reference_date) %>% 
-  summarise(mwis = mean(wis, na.rm = TRUE),
+  left_join(best_nl, by = c("reference_date", "loc_abb", "horizon")) %>% 
+  group_by(model) %>% 
+  summarise(m = mean(lwis < nl_lwis, na.rm = TRUE)) %>% 
+  arrange(desc(m))
+
+team_diffs <- team_scores %>% 
+  filter(model != "asg_disc2_nm_hosp_sq_ar1") %>% 
+  left_join(best_nl, by = c("reference_date", "loc_abb", "horizon")) %>% 
+  group_by(model, loc_abb) %>% 
+  # filter(model != "GH-model") %>% 
+  summarise(mlwis = mean(lwis, na.rm = TRUE),
+            nl_mlwis = mean(nl_lwis, na.rm = TRUE),
+            mwis = mean(wis, na.rm = TRUE),
             nl_mwis = mean(nl_wis, na.rm = TRUE)) %>% 
-  filter(model %in% c("asg_disc2_nm_hosp_sq_ar1", "FluSight-baseline")) %>%
-  ggplot() +
-  geom_line(aes(x = date(reference_date), y = mwis, colour = model))
+  arrange(mwis) %>% 
+  mutate(sldiff = nl_mlwis/mlwis,
+         sdiff = nl_mwis/mwis)  
 
-m_scores <- team_scores %>% 
-  # filter(location_name == "US") %>%
-  group_by(model, location_name) %>% 
-  summarise(mrwis = median(wis, na.rm = TRUE)) 
+scores_ord <- team_scores %>% 
+  filter(model != "asg_disc2_nm_hosp_sq_ar1") %>%
+  left_join(best_nl, by = c("reference_date", "loc_abb", "horizon")) %>% 
+  group_by(model) %>% 
+  # filter(model != "GH-model") %>% 
+  summarise(mlwis = mean(lwis, na.rm = TRUE),
+            nl_mlwis = mean(nl_lwis, na.rm = TRUE),
+            mwis = mean(wis, na.rm = TRUE),
+            nl_mwis = mean(nl_wis, na.rm = TRUE)) %>% 
+  arrange(mwis) %>% 
+  mutate(sldiff = nl_mlwis/mlwis,
+         sdiff = nl_mwis/mwis) %>% 
+  arrange(sldiff)
 
+scores_ord$model
 
-m_scores %>% 
-  filter(model != "GH-model") %>% 
-  filter(mrwis < 200) %>% 
+state_ord <- team_scores %>% 
+  left_join(best_nl, by = c("reference_date", "loc_abb", "horizon")) %>% 
+  group_by(loc_abb) %>% 
+  # filter(model != "GH-model") %>% 
+  summarise(mlwis = mean(lwis, na.rm = TRUE),
+            nl_mlwis = mean(nl_lwis, na.rm = TRUE),
+            mwis = mean(wis, na.rm = TRUE),
+            nl_mwis = mean(nl_wis, na.rm = TRUE)) %>% 
+  arrange(mwis) %>% 
+  mutate(sldiff = nl_mlwis/mlwis,
+         sdiff = nl_mwis/mwis) %>% 
+  arrange(sldiff)
+
+state_ldiff <- team_diffs %>% 
+  filter(model != "asg_disc2_nm_hosp_sq_ar1") %>%
+  mutate(model = factor(model, levels = scores_ord$model),
+         loc_abb = factor(loc_abb, levels = state_ord$loc_abb)) %>% 
+  mutate(diff_lim = ifelse(sdiff < -1, 
+                           #-max(team_diffs$sdiff), 
+                           -1, 
+                           #-max(team_diffs$sdiff), 
+                           sdiff)) %>% 
   ggplot() +
   geom_tile(aes(y = model,
-                x = location_name,
-                fill = mrwis)) +
+                x = loc_abb,
+                fill = sldiff)) +
   scale_fill_gradient2(
-    #low = "#5ab4ac", high = "#d8b365", mid = "white",
-    # low = "#5ab4ac", high = "black", mid = "white",
-    low = "navyblue", high = "red", mid = "white",
-    midpoint = 1, #mean(m_scores$mrwis, na.rm = TRUE),
-    # limits = c(min(m_scores$mrwis), max(m_scores$mrwis)),
-    na.value = NA) #+
-  # coord_flip() +
-  labs(fill = "LWIS") +
-  ylab("Week") +
+    low = "royalblue", high = "red", mid = "white",
+    midpoint = 1) +
+  scale_x_discrete(expand = c(0,0)) +
+  scale_y_discrete(expand = c(0,0)) +
   xlab("") +
+  ylab("LWIS") +
+  theme(panel.grid = element_blank(),
+        panel.background = element_rect(fill = "grey50", color = NA),
+        # legend.position = "none",
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        legend.title = element_blank(),
+        # legend.position = "none",
+        # axis.text.y = element_text(size = 10, angle = 20),
+        axis.title = element_text(size = 16),
+        axis.text.y = element_markdown())
+
+
+
+state_diff <- team_diffs %>% 
+  filter(model != "asg_disc2_nm_hosp_sq_ar1") %>%
+  mutate(model = factor(model, levels = scores_ord$model),
+         loc_abb = factor(loc_abb, levels = state_ord$loc_abb)) %>% 
+  mutate(diff_lim = ifelse(sdiff < -1, 
+                           #-max(team_diffs$sdiff), 
+                           -1, 
+                           #-max(team_diffs$sdiff), 
+                           sdiff)) %>% 
+  ggplot() +
+  geom_tile(aes(y = model,
+                x = loc_abb,
+                fill = sdiff)) +
+  scale_fill_gradient2(
+    low = "royalblue", high = "red", mid = "white",
+    midpoint = 1) +
+  scale_x_discrete(expand = c(0,0)) +
+  scale_y_discrete(expand = c(0,0)) +
+  xlab("Location") +
+  ylab("WIS") +
+  theme(panel.grid = element_blank(),
+        panel.background = element_rect(fill = "grey50", color = NA),
+        # legend.position = "none",
+        axis.text.x = element_text(size = 7, angle = 90),
+        legend.title = element_blank(),
+        # legend.position = "none",
+        # axis.text.y = element_text(size = 10, angle = 20),
+        axis.title = element_text(size = 16),
+        axis.text.y = element_markdown())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+team_ddiffs <- team_scores %>% 
+  filter(model != "asg_disc2_nm_hosp_sq_ar1") %>% 
+  left_join(best_nl, by = c("reference_date", "loc_abb", "horizon")) %>% 
+  group_by(model, reference_date) %>% 
+  # filter(model != "GH-model") %>% 
+  summarise(mlwis = mean(lwis, na.rm = TRUE),
+            nl_mlwis = mean(nl_lwis, na.rm = TRUE),
+            mwis = mean(wis, na.rm = TRUE),
+            nl_mwis = mean(nl_wis, na.rm = TRUE)) %>% 
+  arrange(mwis) %>% 
+  mutate(sldiff = nl_mlwis/mlwis,
+         sdiff = nl_mwis/mwis) %>% 
+  filter(model != "asg_disc2_nm_hosp_sq_ar1") %>%
+  mutate(model = factor(model, levels = scores_ord$model)) %>% 
+  mutate(diff_lim = ifelse(sdiff < -1, 
+                           #-max(team_diffs$sdiff), 
+                           -1, 
+                           #-max(team_diffs$sdiff), 
+                           sdiff))
+
+library(scales)
+library(ggtext)
+# labels <- levels(team_ddiffs$model)
+# labels[labels == "FluSight-baseline"] <- "<b>FluSight-baseline</b>"
+date_ldiff <- team_ddiffs %>%
+  mutate(model = factor(model, levels = scores_ord$model)) %>%  
+  ggplot() +
+  geom_tile(aes(y = model,
+                x = date(reference_date),
+                fill = sldiff)) +
+  scale_fill_gradient2(
+    low = "royalblue", high = "red", mid = "white",
+    midpoint = 1) +
+  scale_x_date(expand = c(0,0),
+               breaks = pretty_breaks(n = 6)) +
+  scale_y_discrete(expand = c(0,0)) +
+  ylab("LWIS") +
+  xlab("") +
+  theme(panel.grid = element_blank(),
+        panel.background = element_rect(fill = "grey50", color = NA),
+        legend.title = element_blank(),
+        # legend.position = "none",
+        # axis.text.x = element_text(size = 11),
+        # legend.position = "none",
+        # axis.text.y = element_text(size = 10, angle = 20),
+        axis.title = element_text(size = 16),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.text.y = element_markdown())
+        # axis.text.y = element_blank(),
+        # axis.ticks.y = element_blank())
+
+
+
+date_diff <- team_ddiffs %>%
+  mutate(model = factor(model, levels = scores_ord$model)) %>%  
+  ggplot() +
+  geom_tile(aes(y = model,
+                x = date(reference_date),
+                fill = sdiff)) +
+  scale_fill_gradient2(
+    low = "royalblue", high = "red", mid = "white",
+    midpoint = 1) +
+  scale_x_date(expand = c(0,0),
+               breaks = pretty_breaks(n = 6)) +
+  scale_y_discrete(expand = c(0,0)) +
+  ylab("WIS") +
+  xlab("Date") +
+  theme(panel.grid = element_blank(),
+        panel.background = element_rect(fill = "grey50", color = NA),
+        # legend.position = "none",
+        axis.text.x = element_text(size = 11),
+        legend.title = element_blank(),
+        # legend.position = "none",
+        # axis.text.y = element_text(size = 10, angle = 20),
+        axis.title = element_text(size = 16),
+        # axis.text.y = element_blank(),
+        # axis.ticks.y = element_blank()
+        axis.text.y = element_markdown())
+
+
+
+
+library(gridExtra)
+library(grid)
+
+grid.arrange(state_ldiff, date_ldiff, state_diff, date_diff, 
+             ncol = 2, 
+             # left = textGrob("Model", 
+             #                 rot = 90, vjust = 1, 
+             #                 gp = gpar(fontsize = 16)),
+             widths = c(1.5, 1))
+
+state_ldiff / state_diff
+date_ldiff / date_diff
+
+##################################
+#############Coverage#############
+##################################
+# saveRDS(ili_coverage, "./nl_cover.rds")
+team_coverage <- readRDS("./hub_cover_no_ens.rds")
+# ili_coverage <- readRDS("./nl_cover.rds")
+p3 <- team_coverage %>% 
+  filter(!str_detect(model, "FluSight-baseline")) %>% 
+  dplyr::select(reference_date, horizon, location, model, contains("cover")) %>% 
+  pivot_longer(contains("cover"), names_to = "coverage", 
+               values_to = "covered") %>% 
+  filter(horizon >= 0 & !is.na(covered)) %>% 
+  mutate(coverage = .01*as.numeric(str_extract(coverage, "\\d+"))) %>% 
+  group_by(model, coverage) %>% 
+  summarise(pcover = mean(covered)) %>% 
+  # filter(model == "FluSight-baseline") %>% 
+  ggplot(aes(x = coverage, y = pcover, line_type = model)) +
+  geom_line(colour = "grey", size = .9) +
+  geom_abline(intercept = 0, slope = 1, size = 1.2) +
+  geom_line(data = ili_coverage %>%
+              filter(model == "asg_disc2_nm_hosp_sq_ar1") %>%
+              mutate(model = "ASG") %>% 
+              dplyr::select(reference_date, horizon, location, model, contains("cover")) %>% 
+              pivot_longer(contains("cover"), names_to = "coverage", 
+               values_to = "covered") %>% 
+              filter(horizon >= 0 & !is.na(covered)) %>% 
+              mutate(coverage = .01*as.numeric(str_extract(coverage, "\\d+"))) %>% 
+              group_by(model, coverage) %>% 
+              summarise(pcover = mean(covered)),
+            aes(x = coverage, y = pcover, linetype = model, colour = model), 
+            size = 1.2) +
+  geom_line(data = team_coverage %>%
+              filter(str_detect(model, "FluSight-baseline")) %>%
+              mutate(model = "FluSight-baseline") %>% 
+              dplyr::select(reference_date, horizon, location, model, contains("cover")) %>% 
+              pivot_longer(contains("cover"), names_to = "coverage", 
+                           values_to = "covered") %>% 
+              filter(horizon >= 0 & !is.na(covered)) %>% 
+              mutate(coverage = .01*as.numeric(str_extract(coverage, "\\d+"))) %>% 
+              group_by(model, coverage) %>% 
+              summarise(pcover = mean(covered)),
+            aes(x = coverage, y = pcover, linetype = model, colour = model), 
+            size = 1.2) +
+  scale_color_manual(values = c("ASG" = "blue", "FluSight-baseline" = "red")) +
+  scale_linetype_manual(values = c("ASG" = "dashed", 
+                                   "FluSight-baseline" = "twodash")) +
   
+  xlab("Theoretical Coverage") +
+  ylab("Empirical Coverage") +
   theme_bw() +
-  theme(panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank(),
-        axis.text.x=element_text(size=16),
-        axis.text.y = element_text(size = 14, angle = 90, hjust = .45),
-        axis.title=element_text(size=20),
-        strip.text = element_text(
-          size = 13),
-        legend.text = element_text(size = 12), 
-        legend.title = element_text(size = 14),
-        legend.position = "none")
+  theme(legend.position = c(.2, .8),
+        legend.title = element_blank(),
+        axis.title = element_text(size = 16),
+        axis.text = element_text(size = 10),
+        legend.text = element_text(size = 12))
+
+
+team_scores %>% 
+  filter(str_detect(model, "asg") | str_detect(model, "FluSight-baseline")) %>% 
+  mutate(model = ifelse(str_detect(model, "asg"), "ASG", 
+                                   "FluSight-baseline")) %>% 
+  group_by(model, reference_date) %>% 
+  summarise(mlwis = mean(lwis, na.rm = TRUE),
+            mwis = mean(wis, na.rm = TRUE)) %>% 
+  ggplot() +
+  geom_line(aes(x = date(reference_date), y = mlwis, 
+                colour = model, linetype = model), size = .9) +
+  scale_colour_manual(values = c("ASG" = "blue", 
+                                 "FluSight-baseline" = "red")) +
+  xlab("") +
+  ylab("LWIS") +
+  theme_bw() +
+  theme(legend.position = c(.2, .89),
+        legend.title = element_blank(),
+        axis.title = element_text(size = 16),
+        axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 12),
+        legend.text = element_text(size = 12))
+
+team_scores %>% 
+  filter(str_detect(model, "asg") | str_detect(model, "FluSight-baseline")) %>% 
+  mutate(model = ifelse(str_detect(model, "asg"), "ASG", 
+                        "FluSight-baseline")) %>% 
+  group_by(model, reference_date) %>% 
+  summarise(mlwis = mean(lwis, na.rm = TRUE),
+            mwis = mean(wis, na.rm = TRUE)) %>% 
+  ggplot() +
+  geom_line(aes(x = date(reference_date), y = mlwis, 
+                colour = model, linetype = model), size = .9) +
+  scale_colour_manual(values = c("ASG" = "blue", 
+                                 "FluSight-baseline" = "red")) +
+  xlab("Date") +
+  ylab("WIS") +
+  theme_bw() +
+  theme(legend.position = c(.2, .89),
+        legend.title = element_blank(),
+        axis.title = element_text(size = 16),
+        axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 12),
+        legend.text = element_text(size = 12))
+
+
+
+
+library(patchwork)
+
+p1 <- rbind(team_scores, ili_scores) %>% 
+  filter(model %in% c("asg_disc2_nm_hosp_sq_ar1") |
+           str_detect(model, "FluSight")) %>% 
+  mutate(model = ifelse(str_detect(model, "asg"), "ASG",
+                                   "FluSight-baseline")) %>% 
+  select(model, horizon, lwis, wis) %>% 
+  pivot_longer(cols = c("wis", "lwis"), 
+               names_to = "score", values_to = "wis") %>% 
+  mutate(score = ifelse(score == "wis", "WIS", "LWIS")) %>% 
+  select(model, score, wis, horizon) %>% unique() %>% 
+  filter(score == "LWIS") %>%
+  ggplot() +
+  geom_boxplot(aes(x = factor(horizon + 1), y = wis, colour = model)) +
+  scale_colour_manual(values = c("ASG" = "blue", "FluSight-baseline" = "red")) +
+  # scale_y_log10() +
+  xlab("") +
+  ylab("LWIS") +
+  # facet_wrap(~score, scales = "free_y") +
+  theme_bw() +
+  theme(legend.title = element_blank(),
+        legend.position = c(.15, .75),
+        legend.text = element_text(size = 13),
+        axis.title = element_text(size = 16),
+        axis.text = element_text(size = 12))
+
+
+
+p2 <- rbind(team_scores, ili_scores) %>% 
+  filter(model %in% c("asg_disc2_nm_hosp_sq_ar1") |
+           str_detect(model, "FluSight")) %>% 
+  mutate(model = ifelse(str_detect(model, "asg"), "ASG",
+                        "FluSight-baseline")) %>% 
+  select(model, horizon, lwis, wis) %>% 
+  pivot_longer(cols = c("wis", "lwis"), 
+               names_to = "score", values_to = "wis") %>% 
+  mutate(score = ifelse(score == "wis", "WIS", "LWIS")) %>% 
+  select(model, score, wis, horizon) %>% unique() %>% 
+  filter(score == "WIS") %>%
+  ggplot() +
+  geom_boxplot(aes(x = factor(horizon + 1), y = wis, colour = model)) +
+  scale_colour_manual(values = c("ASG" = "blue", "FluSight-baseline" = "red")) +
+  scale_y_log10() +
+  xlab("Horizon") +
+  ylab("WIS") +
+  # facet_wrap(~score, scales = "free_y") +
+  theme_bw() +
+  theme(legend.title = element_blank(),
+        legend.position = "none", #c(.15, .75),
+        legend.text = element_text(size = 14),
+        axis.title = element_text(size = 16),
+        axis.text = element_text(size = 12))
+
+
+
+(p1 / p2)
+
+
+
+rbind(team_coverage, ili_coverage) %>% 
+  # filter(!str_detect(model, "FluSight-baseline")) %>% 
+  dplyr::select(reference_date, horizon, location, model, contains("cover")) %>% 
+  pivot_longer(contains("cover"), names_to = "coverage", 
+               values_to = "covered") %>% 
+  filter(horizon >= 0 & !is.na(covered)) %>% 
+  mutate(coverage = .01*as.numeric(str_extract(coverage, "\\d+"))) %>% 
+  group_by(model, coverage) %>% 
+  summarise(pcover = mean(covered)) %>%
+  group_by(model) %>% 
+  summarise(m = mean((coverage - pcover)^2)) %>% 
+  arrange(m)
+
+
+
--- make_images/make_scores_comp.R
+++ make_images/make_scores_comp.R
@@ -237,23 +237,97 @@ team_coverage %>%
   stat_function(fun = function(x) x + 0, color = "blue", size = 1.2)
   
 
-
+team_scores <- readRDS("~/flu_research/nonlinear_flu_forecast/flu_forecast_23/hub_scores_no_ens.rds")
 
 setwd("../../../flu_research/nonlinear_flu_forecast/flu_forecast_23/fin_hosp_forecasts/")
 
 folders <- list.files("./")
-folders <- folders[!str_detect(folders, "full") & 
+folders <- folders[!str_detect(folders, "full") &
                    !str_detect(folders, "_disc_")]
-folders <- folders[!str_detect(folders, "disc2") | 
-                     str_detect(folders, "disc2_nm")]
+# folders <- folders[!str_detect(folders, "disc2") |
+#                      str_detect(folders, "disc2_nm")]
 
+folders <- folders[(str_detect(folders, "asg_nm")
+                   | str_detect(folders, "asg_disc2_nm")
+                   | str_detect(folders, "sir_hosp")
+                   | str_detect(folders, "sir_disc2"))
+                   & !str_detect(folders, "logi")
+                   & !str_detect(folders, "tr")]
+
+# folders <- folders[str_detect(folders, "asg_disc2_ho") & !str_detect(folders, "tr")]
+# folders <- folders[str_detect(folders, "asg_disc2_nm") & !str_detect(folders, "tr")]
 models <- folders
+# models <- "asg_disc2_nm_hosp_lst_ar1"
+# most_coverage <- ili_coverage
+ili_coverage <- data.frame()
 
+for (sub_date in sub_dates) {
+  for (i in 1:length(models)) {
+    forecast_file <- paste(models[i], "/", sub_date, "-",
+                           models[i], ".csv", sep = "")
+    
+    if (file.exists(forecast_file) == FALSE) next
+    forecast <- read.csv(forecast_file) %>%
+      mutate(location = as.character(location),
+             output_type_id = as.character(output_type_id)) %>%
+      mutate(location = ifelse(nchar(location) < 2, paste0("0",location),
+                               location)) %>%
+      mutate(value = ifelse(value < 0, 0, value)) %>%
+      filter(output_type == "quantile") %>%
+      mutate(location = as.character(location)) %>%
+      # filter(output_type_id %in% c(.025, .25, .75, .975)) %>%
+      pivot_wider(names_from = output_type_id, values_from = value) %>%
+      rename(c(lower98 = "0.01",
+               lower95 = "0.025", 
+               lower90 = "0.05",
+               lower80 = "0.1",
+               lower70 = "0.15",
+               lower60 = "0.2",
+               lower50 = "0.25",
+               lower40 = "0.3",
+               lower30 = "0.35",
+               lower20 = "0.4",
+               lower10 = "0.45",
+               upper10 = "0.55",
+               upper20 = "0.6",
+               upper30 = "0.65",
+               upper40 = "0.7",
+               upper50 = "0.75",
+               upper60 = "0.8",
+               upper70 = "0.85",
+               upper80 = "0.9",
+               upper90 = "0.95",
+               upper95 = "0.975",
+               upper98 = "0.99"))
+    
+    for_cover <- forecast %>%
+      left_join(target_data,
+                by = c("location", "target_end_date" = "date")) %>%
+      mutate(true_value = as.numeric(true_value))
+    
+    cover <- for_cover %>%
+      mutate(cover10 = between(true_value, lower10, upper10),
+             cover20 = between(true_value, lower20, upper20),
+             cover30 = between(true_value, lower30, upper30),
+             cover40 = between(true_value, lower40, upper40),
+             cover50 = between(true_value, lower50, upper50),
+             cover60 = between(true_value, lower60, upper60),
+             cover70 = between(true_value, lower70, upper70),
+             cover80 = between(true_value, lower80, upper80),
+             cover90 = between(true_value, lower90, upper90),
+             cover95 = between(true_value, lower95, upper95),
+             cover98 = between(true_value, lower98, upper98)) %>%
+      mutate(model = models[i])
+    
+    ili_coverage <- rbind(ili_coverage, cover)
+    
+    
+  }
+}
 
-
-
-
-
+# saveRDS(most_scores, "~/flu_research/nonlinear_flu_forecast/flu_forecast_23/nl_scores.rds")
+# saveRDS(most_coverage, "~/flu_research/nonlinear_flu_forecast/flu_forecast_23/nl_cover.rds")
+# most_scores <- ili_scores
 ili_scores <- data.frame()
 for (sub_date in sub_dates) {
   for (i in 1:length(models)) {
@@ -270,7 +344,7 @@ for (sub_date in sub_dates) {
       mutate(location = as.character(location)) %>% # %>% select(reference_date, horizon, target, target_end_date, location, output_type, output_type_id, value)
       mutate(value = ifelse(value < 0, 0, value)) #%>%
     # mutate(target_end_date = as.character(date(target_end_date) - 7))
-    
+    if (sum(is.na(forecast$value)) > 0) {next}
     for_wis <- forecast %>%
       filter(horizon %in% 0:3) %>%
       left_join(target_data,
@@ -309,18 +383,102 @@ for (sub_date in sub_dates) {
   print(sub_date)
 }
 
+# saveRDS(ili_scores, "~/flu_research/nonlinear_flu_forecast/flu_forecast_23/nl_scores.rds")
+# rbind(ili_scores, team_scores) %>% 
+ili_scores %>% 
+  group_by(model) %>% 
+  filter(horizon != -1) %>% 
+  unique() %>% 
+  filter(!str_detect(model, "tr_")) %>% #, location_name != "Florida") %>% 
+  filter(!is.na(wis)) %>% 
+  summarise(m = mean(wis, na.rm = TRUE),
+            n = n()) %>%
+  # filter(str_detect(model, "asg_nm")) %>%
+  arrange(m) %>% 
+  as.data.frame()
+
+
+rbind(ili_scores, team_scores) %>% 
+  ggplot() +
+  geom_boxplot(aes(x = model, y = wis)) +
+  scale_y_log10()
+
+asgbase <- scores %>% 
+  filter(model == "asg_disc2_nm_hosp_sq_ar1") %>% 
+  mutate(all = paste(reference_date, location_name, horizon))
+
+base <- ili_scores %>% 
+  filter(model == "asg_disc2_hosp_sq_ar1") %>% 
+  mutate(all = paste(reference_date, location_name, horizon))
+
+
+scores %>% 
+  mutate(all = paste(reference_date, location_name, horizon)) %>% 
+  filter(all %in% base$all) %>% 
+  group_by(model) %>% 
+  summarise(m = mean(wis, na.rm = TRUE)) %>% 
+  arrange(m)
+
+
+ili_scores %>% 
+  filter(str_detect(model, "asg_disc2_nm")) %>% 
+  ggplot() +
+  geom_boxplot(aes(x = lwis)) + 
+  facet_wrap(~model, scales = "free")
+
+ili_scores %>% 
+  filter(!str_detect(model, "tr_")) %>%
+  group_by(model) %>% 
+  mutate(max = wis == max(wis, na.rm = TRUE)) %>% 
+  filter(max != TRUE) %>% 
+  group_by(model) %>% 
+  summarise(m = mean(wis)) %>% 
+  arrange(m) %>% View()
+
+ili_scores <- ili_scores %>% 
+  mutate(all = paste(location_name, reference_date, horizon, sep = "_"))
+
+asg_scores <- ili_scores %>% 
+  filter(model == "asg_disc2_nm_hosp_sq_ar1")
+
+asg_lst_scores <- ili_scores %>% 
+  filter(model == "asg_disc2_nm_hosp_lst_ar1")
+
+
+sscores <- team_scores %>% 
+  filter(model == "SigSci-CREG") %>% 
+  mutate(all = paste(reference_date, location_name, horizon, sep = "_"))
+# team_scores <- readRDS("./hub_scores_no_ens.rds")
 # saveRDS(team_scores, "../hub_scores_no_ens.rds")
 # saveRDS(ili_scores, "../nl_scores.rds")
-rbind(team_scores, ili_scores) %>% 
-  filter(str_detect(model, "asg")) %>%
-  filter(location_name != "Florida") %>% 
+
+rbind(team_scores, ili_scores) %>% # %>% filter(model == "asg_disc2_nm_hosp_sq_ar1")) %>% 
+  # filter(str_detect(model, "asg")) %>%
+  # filter(location_name != "Florida") %>% 
+  # mutate(all = paste(reference_date, location_name, horizon, sep = "_")) %>%
+  # filter(all %in% unique(sscores$all)) %>%
   group_by(model) %>% 
   summarise(m = mean(lwis, na.rm = TRUE),
             n = n()) %>% 
   arrange(m)
 
+ili_scores %>% 
+  filter(str_detect(model, "asg") #& str_detect(model, "nm")
+         & !str_detect(model, "logi") & str_detect(model, "lst")) %>% 
+  group_by(model) %>% 
+  summarise(n = n()) %>% 
+  arrange(model)
 
-team_coverage <- data.frame()
+
+ili_scores %>% 
+  # filter((str_detect(model, "sir_hosp") | str_detect(model, "sir_disc2"))
+  #        & !str_detect(model, "logi")) %>% 
+  filter(str_detect(model, "sir")) %>% 
+  group_by(model) %>% 
+  summarise(n = n()) %>% 
+  arrange(model)
+
+ili_coverage <- data.frame()
 
 for (sub_date in sub_dates) {
   for (i in 1:length(models)) {
@@ -347,7 +505,7 @@ for (sub_date in sub_dates) {
              cover95 = between(true_value, lower95, upper95)) %>%
       mutate(model = models[i])
     
-    team_coverage <- rbind(team_coverage, cover)
+    ili_coverage <- rbind(ili_coverage, cover)
     
     
   }
